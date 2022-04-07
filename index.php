e8bd39888c0d7d6e6f1538d91fe79729748ed066
Martino888 <maxmartino33@gmail.com>
2022-04-07 16:03:46 +0200

    inserimento paragrafo

--- index.php
+++ index.php
@@ -7,7 +7,21 @@
     <title>PHP</title>
 </head>
 <body>
-<?php $name=mario; ?>
-    ciao<?php echo $name; ?>
+<?php $cafe="Na' tazzulella e' cafè e mai niente cè fanno sapè
+nui cè puzzammo e famme, o sanno tutte quante
+e invece e c'aiutà c'abboffano e' cafè
+Na' tazzulella e' cafè ca sigaretta a coppa pe nun verè
+che stanno chine e sbaglie, fanno sulo mbruoglie
+s'allisciano se vattono se pigliano o' cafè
+E nui passammo e uaie e nun puttimmo suppurtà
+e chiste invece e rà na mano s'allisciano se vattono se
+magniano a città
+Na' tazzulella e' cafè acconcia a vocca a chi nun po'sapè
+e nui tirammo annanz che rulore e' panze
+e invece e c'aiutà c'abboffano e' cafè
+Na' tazzulella e' cafè ca' sigaretta a coppa pè nun verè
+s'aizano e' palazze fanno cose e' pazze ci girano
+c'avotano ci iengono e' tasse.."; ?>
+<?php echo $cafe; ?>
 </body>
 </html>
